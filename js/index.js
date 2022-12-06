a98bd1ba1d0c19f27343b57125799f1335967c5c
Sergey <semichev-serzh@mail.ru>
2022-12-06 18:46:11 +0300

    second commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,2 @@
-document.getElementById('sign-in-btn').addEventListener('click', e => {
-    document.location.href = 'html/sign-in.html';
-})
-
-document.getElementById('sign-up-btn').addEventListener('click', e => {
-    document.location.href = 'html/sign-up.html';
-})
+makeRelocateButton('sign-in-btn', 'html/sign-in.html')
+makeRelocateButton('sign-up-btn', 'html/sign-up.html')
--- js/utils.js
+++ js/utils.js
@@ -1,31 +1,5 @@
-class LocalStorageManager {
-    constructor() {
-        this.constants = {
-            'TOKEN_KEY': 'Token',
-            'ID_KEY': 'Id',
-            'EMAIL_KEY': 'Email',
-            'AUTHORITIES_KEY': 'Authorities'
-        }
-    }
-    saveToken(token) {
-        window.localStorage.removeItem(this.constants.TOKEN_KEY)
-        window.localStorage.setItem(this.constants.TOKEN_KEY, token)
-    }
-    saveId(id) {
-        window.localStorage.removeItem(this.constants.ID_KEY)
-        window.localStorage.setItem(this.constants.ID_KEY, id)
-    }
-    saveEmail(email) {
-        window.localStorage.removeItem(this.constants.EMAIL_KEY)
-        window.localStorage.setItem(this.constants.EMAIL_KEY, email)
-    }
-    saveAuthorities(authorities) {
-        window.localStorage.removeItem(this.constants.AUTHORITIES_KEY)
-        window.localStorage.setItem(this.constants.AUTHORITIES_KEY, JSON.stringify(authorities))
-    }
-    signOut() {localStorage.clear()}
-    getToken() {return localStorage.getItem(this.constants.TOKEN_KEY)}
-    getId() {return localStorage.getItem(this.constants.ID_KEY)}
-    getEmail() {return localStorage.getItem(this.constants.EMAIL_KEY)}
-    getAuthorities() {return localStorage.getItem(this.constants.AUTHORITIES_KEY)}
+function makeRelocateButton(buttonId, pageUrl) {
+    document.getElementById(buttonId).addEventListener('click', () => {
+        document.location.href = pageUrl
+    })
 }
